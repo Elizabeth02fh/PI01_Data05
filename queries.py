49828b153808d56c1dee768eb0940627b175f8a3
Luis Martínez <98825383+Jozslu@users.noreply.github.com>
2023-04-18 13:55:16 -0500

    Update queries.py

--- queries.py
+++ queries.py
@@ -1,3 +1,5 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 import pandas as pd 
 import numpy as np 
 
@@ -78,3 +80,29 @@ def get_contents(rating: str):
 
     return {"rating" : rating, "contenido" : q6}
 #print(get_contents("13+"))
+
+### # Sistema de recomendación:
+# Elimina las palabras comunes en inglés
+vector = TfidfVectorizer(stop_words = "english")
+
+# Cargamos el dataset
+data = pd.read_csv("data_general.csv", index_col= False, sep= ",", header= 0)
+
+data["description"] = data["description"].replace(np.nan,"")
+
+vector_matriz = vector.fit_transform(data["description"])
+coseno = linear_kernel(vector_matriz, vector_matriz)
+indices = pd.Series(data.index, index= data["title"]).drop_duplicates() 
+
+def get_recomendation(title, coseno = coseno):
+    id = indices[title]
+
+    x1 = list(enumerate(coseno[id])) 
+    x1 = sorted(x1, key= lambda x: x[1], reverse= True)
+    # Obtenemos los 5 primeros.
+    x1 = x1[1:6]
+
+    movie_index = [i[0] for i in x1]
+    # Devolvera los 3 primeros.
+    return data["title"].iloc[movie_index].to_list()[:3] 
+print(get_recomendation("the grand seduction")
